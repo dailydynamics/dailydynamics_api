5d385150109ac043b5f17be98834ef0906497901
dailydynamics <dev.dailydynamics@gmail.com>
2022-09-17 19:30:37 +0530

    :lipstick: Blade Components

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('auth.login');
+    }
+    public function forgotpswd()
+    {
+        return view('auth.forgot');
+    }
+    public function loginPost(Request $request)
+    {
+        return redirect()->route('dashboard');
+    }
+    public function forgotPost(Request $request)
+    {
+        return "FORGOT POST";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::name('auth.')->group(function () {
+    Route::get('login', [AuthController::class, 'login'])->name('login');
+    Route::post('login', [AuthController::class, 'loginPost'])->name('login');
+    Route::get('forgot-password', [AuthController::class, 'forgotpswd'])->name('forgotpswd');
+    Route::post('forgot-password', [AuthController::class, 'forgotPost'])->name('forgotpswd');
+});
+
+
+Route::get('dashboard', function () {
+    return view('dashboard.index');
+})->name('dashboard');
