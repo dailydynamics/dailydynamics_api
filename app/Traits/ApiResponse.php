1af21f8b7f061f28e4411f8bac7042431e58805e
dailydynamics <dev.dailydynamics@gmail.com>
2022-11-19 09:39:27 +0530

    :hammer: Trait Added

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponse
+{
+    protected function success($data = null, $message = 'Success', $code = 200)
+    {
+        return response()->json(['status' => 'success', 'code' => $code, 'message' => $message, 'data' => $data], $code);
+    }
+    protected function error($message = 'Error', $code = 402)
+    {
+        return response()->json(['message' => $message, 'code' => $code, 'status' => 'error'], $code);
+    }
+}
